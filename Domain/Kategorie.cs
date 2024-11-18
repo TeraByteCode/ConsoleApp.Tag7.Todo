7fca5443c35908d6d866887b34e2f3922e04787d
Victor <santana@webmatik.ch>
2024-11-18 23:02:10 +0100

    add: Kategorie property

--- Domain/Kategorie.cs
+++ Domain/Kategorie.cs
@@ -2,5 +2,12 @@
 
 public class Kategorie
 {
-
+    public string Bezeichnung { get; set; }
+    public string Beschreibung { get; set; }
+    public bool IstAktiv { get; set; }
+    public DateTime Erstellungsdatum { get; set; }
+    public DateTime Aenderungsdatum { get; set; }
+    public Person Ersteller { get; set; }
+    public Person Bearbeiter { get; set; }
+    public List<Todo> Todos { get; set; }
 }
